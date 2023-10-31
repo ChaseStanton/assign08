20c54029fdf18e81817445d23970936a630e744c
Reece Kalmar <ralmarkalmar@gmail.com>
2023-10-30 18:30:12 -0600

    s

---
+++ BinaryNode.java
@@ -0,0 +1,41 @@
+package assign08;
+
+public class BinaryNode <Type> {
+    private Type data;
+    private BinaryNode<Type> leftChild, rightChild;
+
+    public BinaryNode(Type data){
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public BinaryNode(Type data, BinaryNode<Type> leftChild, BinaryNode<Type> rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+
+    public void setData(Type data){
+        this.data = data;
+    }
+
+    public void setLeftChild(BinaryNode<Type> leftChild){
+        this.leftChild = leftChild;
+    }
+
+    public void setRightChild(BinaryNode<Type> rightChild){
+        this.rightChild = rightChild;
+    }
+
+    public BinaryNode<Type> getLeftChild(){
+        return this.leftChild;
+    }
+
+    public BinaryNode<Type> getRightChild(){
+        return this.rightChild;
+    }
+    public Type getData(){
+        return this.data;
+    }
+}
---
+++ BinarySearchTree.java
@@ -0,0 +1,5 @@
+package assign08;
+
+public class BinarySearchTree<Type extends Comparable<? super Type>> implements SortedSet<Type> {
+    
+}
