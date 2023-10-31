7512219558c56aef8e1c88d90f8fad1dc4c490c3
ChaseStanton <stantonchase4217@gmail.com>
2023-10-31 10:24:36 -0600

    Create BinarySearchTreeTest.java

---
+++ BinarySearchTreeTest.java
@@ -0,0 +1,47 @@
+package assign08;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTreeTest {
+private BinarySearchTree<Integer> tree;
+private List<Integer> list;
+	@BeforeEach
+	void setUp() throws Exception {
+		tree = new BinarySearchTree<>();
+		tree.add(1);
+		tree.add(4);
+		tree.add(10);
+		tree.add(-10);
+		list = new ArrayList<>();
+		list.add(1);
+		list.add(4);
+		list.add(10);
+		list.add(-10);
+	}
+
+	@Test
+	void testContains() {
+		assertTrue(tree.contains(10));
+	}
+	@Test
+	void testContainsAll() {
+		assertTrue(tree.containsAll(list));
+	}
+	@Test
+	void remove() {
+		tree.remove(1);
+		assertTrue(!tree.contains(1));
+		tree.add(1);
+		tree.remove(4);
+		assertTrue(!tree.contains(10));
+	}
+	
+	
+
+}
