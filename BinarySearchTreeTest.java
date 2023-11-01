e0aac6ba9023525c8a3cb377f6c5eb236a0538f2
Reece Kalmar <ralmarkalmar@gmail.com>
2023-11-01 17:53:58 -0600

    Update BinarySearchTreeTest.java

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -9,39 +9,72 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BinarySearchTreeTest {
-private BinarySearchTree<Integer> tree;
-private List<Integer> list;
-	@BeforeEach
-	void setUp() throws Exception {
-		tree = new BinarySearchTree<>();
-		tree.add(1);
-		tree.add(4);
-		tree.add(10);
-		tree.add(-10);
-		list = new ArrayList<>();
-		list.add(1);
-		list.add(4);
-		list.add(10);
-		list.add(-10);
-	}
+    private BinarySearchTree<String> stringTree;
+    private BinarySearchTree<Integer> tree;
+    private List<Integer> list;
 
-	@Test
-	void testContains() {
-		assertTrue(tree.contains(10));
-	}
-	@Test
-	void testContainsAll() {
-		assertTrue(tree.containsAll(list));
-	}
-	@Test
-	void remove() {
-		tree.remove(1);
-		assertTrue(!tree.contains(1));
-		tree.add(1);
-		tree.remove(4);
-		assertTrue(!tree.contains(10));
-	}
-	
-	
+    @BeforeEach
+    void setUp() throws Exception {
+        tree = new BinarySearchTree<>();
+        tree.add(1);
+        tree.add(4);
+        tree.add(10);
+        tree.add(-10);
+        list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(10);
+        list.add(-10);
+    }
+
+    @Test
+    public void testEmptyTree() {
+        stringTree = new BinarySearchTree<String>();
+        assertTrue(stringTree.isEmpty());
+    }
+
+    @Test
+    public void addMethodString() {
+        stringTree = new BinarySearchTree<String>();
+        assertTrue(stringTree.add("Test"));
+        assertTrue(stringTree.contains("Test"));
+    }
+
+    @Test
+    public void addMethodDuplicatesString() {
+        stringTree = new BinarySearchTree<String>();
+        assertTrue(stringTree.add("Test"));
+        assertFalse(stringTree.add("Test"));
+    }
+
+    @Test
+    public void addThreeToTree() {
+        stringTree = new BinarySearchTree<>();
+        assertTrue(stringTree.add("1"));
+        assertTrue(stringTree.add("2"));
+        assertTrue(stringTree.add("3"));
+        assertTrue(stringTree.contains("1"));
+        assertTrue(stringTree.contains("2"));
+        assertTrue(stringTree.contains("3"));
+    }
+
+    @Test
+    void testContains() {
+        assertTrue(tree.contains(10));
+    }
+
+    @Test
+    void testContainsAll() {
+        assertTrue(tree.containsAll(list));
+    }
+
+    @Test
+    void remove() {
+        tree.remove(1);
+        assertFalse(tree.contains(1));
+        tree.add(1);
+        tree.remove(4);
+        assertTrue(tree.contains(10));
+    }
 
 }
